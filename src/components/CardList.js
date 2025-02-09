82f72eeffc2034e4b4f9adafde657027505a55b3
PaulT1983 <pault83@gmail.com>
2025-02-09 21:51:01 +0200

    ErrorBoundry introduction

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Card from "./Card";
 
-const CardList = ( { robots } ) => {
+const CardList = ({ robots }) => {
+  // if (true) {
+  //   throw new Error("I crashed!");
+  // }
   return robots.map((robot, index) => {
     return (
       <Card key={index} id={robot.id} name={robot.name} email={robot.email} />
